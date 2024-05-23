d443ded206da4a9be8cad225a1c6235321fa686e
Tanguy Gellon <tanguy.gellon@gmail.com>
2024-05-23 16:04:12 +0200

    [MATCHES] Started Matches app with models

---
+++ stats/matches/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from userStats.models import User
+
+
+class Match(models.Model):
+    player1 = models.ForeignKey(User, related_name='player1', on_delete=models.CASCADE)
+    player2 = models.ForeignKey(User, related_name='player2', on_delete=models.CASCADE)
+    scoreP1 = models.IntegerField(default=0)
+    scoreP2 = models.IntegerField(default=0)
+    winner = models.ForeignKey(User, related_name='winner', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.player1} vs {self.player2}'
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=100)
+    players = models.ManyToManyField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status_choices = [
+        ('upcoming', 'Upcoming'),
+        ('ongoing', 'Ongoing'),
+        ('completed', 'Completed'),
+    ]
+    status = models.CharField(choices=status_choices, default='upcoming', max_length=50)
+
+    def __str__(self):
+        return self.name
--- stats/microUserStats/settings.py
+++ stats/microUserStats/settings.py
@@ -18,7 +18,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tournament'
 ]
 
 
